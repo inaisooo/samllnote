fd7756a7c678c3cda4ee13378c387bb290d146f6
inaisooo <ejinaiso@163.com>
2024-06-07 16:03:20 +0800

    ad

---
+++ app/src/main/java/com/tianyun/tally/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tianyun.tally;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
